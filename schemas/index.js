fa176adbd512fed54e9a56f0f2786eb8834f5a9d
marivignesh-dev <marivignesh.r@purpleslate.in>
2023-03-04 22:46:12 +0530

    feature: Necessary Schemas declared

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,11 @@
-export const schemaTypes = []
+import testimonials from "./testimonials"
+import abouts from "./abouts"
+import brands from "./brands";
+import contact from "./contact";
+import workExperience from "./workExperience";
+import experiences from "./experiences";
+import skills from "./skills";
+import works from "./works";
+
+
+export const schemaTypes = [testimonials,abouts,brands,contact,workExperience,experiences,skills,works];
